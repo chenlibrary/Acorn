07d9e9d9bf85c05b3cc69ac2274a996c1cbdba31
chenlibrary <one_chenjiahui@163.com>
2022-01-05 14:07:36 +0800

    hot和notice接口

---
+++ src/main/java/com/www/acorn/tool/PageTool.java
@@ -0,0 +1,14 @@
+package com.www.acorn.tool;
+
+import java.util.Date;
+
+public class DayTool {
+
+
+    public String getToDay(){
+
+        Date date = new Date();
+
+        return null;
+    }
+}
